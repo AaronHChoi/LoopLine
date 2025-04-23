a991f0a0da3829b7cea32827d8f7dde7881eb675
julianKim11 <juliankim0911@gmail.com>
2025-04-23 15:00:12 -0300

    Update DevelopmentManager.cs

--- Assets/Scripts/Managers/DevelopmentManager.cs
+++ Assets/Scripts/Managers/DevelopmentManager.cs
@@ -13,12 +13,15 @@ public class DevelopmentManager : MonoBehaviour
     [SerializeField] DialogueManager dialogueManager;
     [SerializeField] TimeManager timeManager;
     [SerializeField] DialogueManager dialManager;
+    [SerializeField] PlayerController playerController;
     bool isCursorVisible = false;
+    bool isUIActive = false;
     private void Awake()
     {
         dialogueManager = FindFirstObjectByType<DialogueManager>();
         timeManager = FindFirstObjectByType<TimeManager>();
         dialManager = FindFirstObjectByType<DialogueManager>();
+        playerController = FindFirstObjectByType<PlayerController>();
     }
     void Start()
     {
@@ -29,8 +32,12 @@ public class DevelopmentManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape) && UIPrinciplal != null && !dialManager.isDialogueActive)
         {
+            isUIActive = !isUIActive;
+
             UIPrinciplal.SetActive(!UIPrinciplal.activeInHierarchy);
             UIDeveloperMode.SetActive(!UIDeveloperMode.activeInHierarchy);
+
+            playerController.SetControllerEnabled(!isUIActive);
 
             UpdateCursorState();
         }
