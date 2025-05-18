f97f39278d7d3b03ce48f1817ea19c9dd4e3b6f0
Furaid <102191127+Furaid@users.noreply.github.com>
2025-05-18 16:28:55 -0300

    emission added

--- Assets/VFX/EagleVision Shader/KeyTarget.hlsl
+++ Assets/VFX/EagleVision Shader/KeyTarget.hlsl
@@ -4,10 +4,18 @@ void KeyTarget_half(
     float3 secondaryColor,
     float3 primaryTexture,
     float3 secondaryTexture,
+    float emission,
     out float3 Out)
 {
     float3 primaryResult = length(primaryTexture) > 0.01 ? primaryTexture : primaryColor;
     float3 secondaryResult = length(secondaryTexture) > 0.01 ? secondaryTexture : secondaryColor;
 
-    Out = keyPressed > 0.5 ? secondaryResult : primaryResult;
+    if (keyPressed > 0.5)
+    {
+        Out = secondaryResult * emission;
+    }
+    else
+    {
+        Out = primaryResult;
+    }
 }
