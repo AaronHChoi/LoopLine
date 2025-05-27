9750019080a37f078bcfc71ce590acb600eeb92b
Mateotorija <mateotorija@gmail.com>
2025-05-27 12:36:02 -0300

    capacidad de correr

--- Assets/Scripts/Player/PlayerModel.cs
+++ Assets/Scripts/Player/PlayerModel.cs
@@ -1,6 +1,7 @@
 public class PlayerModel
 {
     public float Speed { get; private set; } = 3f;
+    public float SprintSpeed { get; private set; } = 6f;
     public float SpeedRotation { get; private set; } = 10f;
     public float LookSensitivity { get; private set; } = 4.5f;
     public bool FocusMode { get; set; } = true;
