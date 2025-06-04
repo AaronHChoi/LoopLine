978dafb8d06248e45dd7615667bcd9dde0224309
Haru <102191668+superbalsosa@users.noreply.github.com>
2025-06-04 00:04:23 -0300

    Sound Manager system base

---
+++ Assets/Scripts/Sound System/SoundData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Assets.Scripts.Sound_System
+{
+    [Serializable]
+    public class SoundData
+    {
+        public AudioClip clip;
+        public AudioMixerGroup mixerGroup;
+        public bool isALoop;
+        public bool isAPlayOnAwake;
+    }
+}
---
+++ Assets/Scripts/Sound System/SoundEmitter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Assets.Scripts.Sound_System
+{
+    public class SoundEmitter : MonoBehaviour
+    {
+
+    }
+}
