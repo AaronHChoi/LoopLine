eed91f69eb24fefb98f0a6c3ac742f71c55fdd99
Haru <102191668+superbalsosa@users.noreply.github.com>
2025-07-22 11:08:56 -0300

    Train Intro Secuence and Sounds
    Added Train pre intro and intro
    Added music
    Added first sound take placeholder

--- Assets/IntroLevelManager.cs
+++ Assets/IntroLevelManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class IntroLevelManager : MonoBehaviour
 {
@@ -8,15 +9,23 @@ public class IntroLevelManager : MonoBehaviour
     [SerializeField] float timeToFadeOut;
     [SerializeField] float timeLevelToFadeIn;
     [SerializeField] float timeToFadeIn;
+    [SerializeField] float timeToChangeLevel;
+    [SerializeField] string nextSceneName;
     void Start()
     {
         fade.PauseFade(timePausedFadeOut);
         fade.FadeOut(timeToFadeOut);
         StartCoroutine(FadeOutOnTime(timeLevelToFadeIn));
+        StartCoroutine(ChangeNextLevel(timeToChangeLevel));
     }
     private IEnumerator FadeOutOnTime(float seconds)
     {
         yield return new WaitForSeconds(seconds);
         fade.FadeIn(timeToFadeIn);
+    }
+    private IEnumerator ChangeNextLevel(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        SceneManager.LoadScene(nextSceneName);
     }
 }
