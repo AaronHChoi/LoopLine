94665b61ec35c6d76fa9589966588dec8ac301b3
Mateotorija <mateotorija@gmail.com>
2025-07-25 21:24:28 -0300

    mas cambios en el inventario

--- Assets/Scripts/InteractSystem/InventorySystem/InventoryUI.cs
+++ Assets/Scripts/InteractSystem/InventorySystem/InventoryUI.cs
@@ -1,7 +1,9 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class InventoryUI : MonoBehaviour
 {
+    [SerializeField] private List<UIInventoryItemSlot> inventorySlots = new List<UIInventoryItemSlot>();
     private void Start()
     {
         PlayerInventorySystem.Instance.OnInventoryChanged += OnUpdateInventory;
@@ -15,6 +17,47 @@ public class InventoryUI : MonoBehaviour
         DrawInventory();
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.WheelUp))
+        {
+            for (int i = 0; i < inventorySlots.Count; i++)
+            {
+                if (inventorySlots[i].gameObject.activeInHierarchy)
+                {
+                    inventorySlots[i].gameObject.SetActive(false);
+                    if (i + 1 < inventorySlots.Count)
+                    {
+                        inventorySlots[i + 1].gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        inventorySlots[0].gameObject.SetActive(true);
+                    }
+                    break;
+                }
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.WheelDown))
+        {
+            for (int i = inventorySlots.Count - 1; i >= 0; i--)
+            {
+                if (inventorySlots[i].gameObject.activeInHierarchy)
+                {
+                    inventorySlots[i].gameObject.SetActive(false);
+                    if (i + 1 >= 0)
+                    {
+                        inventorySlots[i - 1].gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        inventorySlots[inventorySlots.Count - 1].gameObject.SetActive(true);
+                    }
+                    break;
+                }
+            }
+        }
+    }
     public void DrawInventory()
     {
         foreach(ItemInteract item in PlayerInventorySystem.Instance.inventory)
@@ -30,7 +73,7 @@ public class InventoryUI : MonoBehaviour
 
         UIInventoryItemSlot slot = itemUI.GetComponent<UIInventoryItemSlot>();
         slot.Set(item);
-
+        inventorySlots.Add(slot);
     }
 
     public void RemoveInventorySlot(UIInventoryItemSlot item)
