8619a3ebda9c7a9a9447c25e9fbaeb4cbf6bed69
Mateotorija <mateotorija@gmail.com>
2025-09-16 18:20:07 -0300

    .

---
+++ Assets/Scripts/Portal/PortalTeleporter.cs
@@ -0,0 +1,56 @@
+using DependencyInjection;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PortalTeleporter : MonoBehaviour {
+
+	public IPlayerController player;
+	public Transform reciever;
+
+	private bool playerIsOverlapping = false;
+
+    private void Awake()
+    {
+        player = InterfaceDependencyInjector.Instance.Resolve<IPlayerController>();
+    }
+    // Update is called once per frame
+    void Update () {
+		if (playerIsOverlapping)
+		{
+			Vector3 portalToPlayer = player.GetTransform().position - transform.position;
+			float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+			// If this is true: The player has moved across the portal
+			if (dotProduct < 0f)
+			{
+				// Teleport him!
+				float rotationDiff = -Quaternion.Angle(transform.rotation, reciever.rotation);
+				rotationDiff += 180;
+				player.GetTransform().Rotate(Vector3.up, rotationDiff);
+
+				Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+				player.GetTransform().position = reciever.position + positionOffset;
+
+				playerIsOverlapping = false;
+			}
+		}
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			playerIsOverlapping = true;
+		}
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			playerIsOverlapping = false;
+		}
+	}
+}
