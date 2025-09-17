d84297c58f870d161077aeca2b37e74de3cb3ebd
julianKim11 <juliankim0911@gmail.com>
2025-09-17 16:23:40 -0300

    MarkerBoard script

--- Assets/Scripts/MarkerBoard.cs
+++ Assets/Scripts/MarkerBoard.cs
@@ -7,7 +7,13 @@ public class MarkerBoard : MonoBehaviour, IInteract
     [SerializeField] GameObject Photo;
 
     [SerializeField] List<GameObject> clues;
+    [SerializeField] List<GameObject> strings;
     [SerializeField] float delayBetween = 1f;
+    [SerializeField] List<Material> material1;
+    [SerializeField] Material material2;
+    [SerializeField] Material material3;
+    [SerializeField] List<GameObject> objectsToActivate;
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.O))
@@ -28,17 +34,61 @@ public class MarkerBoard : MonoBehaviour, IInteract
     {
         if (GameManager.Instance.CameraGirlPhoto)
         {
-            Photo.SetActive(true);
-            StartCoroutine(ActiveInSequence(clues, delayBetween));
+            //Photo.SetActive(true);
+            StartCoroutine(ChangeMaterialsInSequence(clues, delayBetween));
+            StartCoroutine(ActivateObjectsInSequence(objectsToActivate, delayBetween));
+            StartCoroutine(ChangeMaterialInSequence(strings, material3, 0, delayBetween));
         }
     }
-    IEnumerator ActiveInSequence(List<GameObject> objects, float delay)
+    IEnumerator ChangeMaterialsInSequence(List<GameObject> objects, float delay)
     {
-        foreach (GameObject obj in objects)
+        for (int i = 0; i < objects.Count; i++)
         {
             yield return new WaitForSeconds(delay);
-            if (obj != null)
-                obj.SetActive(true);
+            
+            Renderer rend = objects[i].GetComponent<Renderer>();
+            if (rend != null)
+            {
+                Material[] mats = rend.materials;
+                if (mats.Length > 0 && i < material1.Count)
+                    mats[0] = material1[i];
+
+                mats[1] = material2;
+
+                rend.materials = mats;
+            }
+            if (i == 0)
+                Photo.SetActive(true);
+        }
+    }
+    IEnumerator ChangeMaterialInSequence(List<GameObject> objects, Material newMaterial, int materialIndex, float delay)
+    {
+        for (int i = 0; i < objects.Count; i++)
+        {
+            yield return new WaitForSeconds(delay);
+
+            Renderer rend = objects[i].GetComponent<Renderer>();
+            if (rend != null)
+            {
+                Material[] mats = rend.materials;
+                if (materialIndex >= 0 && materialIndex < mats.Length)
+                {
+                    mats[materialIndex] = newMaterial;
+                    rend.materials = mats;
+                }
+            }
+
+            if (i == 0)
+                Photo.SetActive(true);
+        }
+    }
+    IEnumerator ActivateObjectsInSequence(List<GameObject> objects, float delay)
+    {
+        for (int i = 0; i < objects.Count; i++)
+        {
+            yield return new WaitForSeconds(delay);
+            if (objects[i] != null)
+                objects[i].SetActive(true);
         }
     }
 }
