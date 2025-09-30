08882efc9507dea2121374c57c21385c2660b028
julianKim11 <juliankim0911@gmail.com>
2025-09-30 15:18:11 -0300

    Revert "Update Assets/Scripts/NPCSelector/BoardSelectorController.cs"
    This reverts commit 2fe73b4d2d8818889617f56e70ce25f360213e99.

--- Assets/Scripts/NPCSelector/BoardSelectorController.cs
+++ Assets/Scripts/NPCSelector/BoardSelectorController.cs
@@ -61,11 +61,6 @@ namespace NPCSelector
         /// Create slots dynamically for a given NPC count.
         public void CreateSlots(int npcCount)
         {
-            if (npcCount < 0)
-            {
-                Debug.LogWarning("CreateSlots called with negative npcCount: " + npcCount);
-                return;
-            }
             // Clear old
             foreach (Transform child in slotContainer)
                 Destroy(child.gameObject);
