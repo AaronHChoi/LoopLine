766b0ad7e57e2028b2c84fd44484be2bfc8d2e82
Mateotorija <mateotorija@gmail.com>
2025-10-19 12:28:38 -0300

    AdditiveScene

--- Assets/Scripts/GameSceneManager.cs
+++ Assets/Scripts/GameSceneManager.cs
@@ -1,13 +1,40 @@
+using DependencyInjection;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using DependencyInjection;
 public class GameSceneManager : MonoBehaviour, IGameSceneManager
 {
+    [Header("Scene Settings")]
+    [SerializeField] private List<WeightScene> weightedScenes = new List<WeightScene>();
+
+    [Header("Active Scenes")]
+    private List<string> activeScenes = new List<string>();
+    private List <string> constantActiveScenes = new List<string>();
+ 
     IDialogueManager dialogueManager;
     private void Awake()
     {
         dialogueManager = InterfaceDependencyInjector.Instance.Resolve<IDialogueManager>();
     }
+
+    private void Start()
+    {     
+        foreach (var sceneName in constantActiveScenes)
+        {
+            if (!SceneManager.GetSceneByName(sceneName).isLoaded)
+            {
+                SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+            }
+        }
+    }
+
+    public void LoadRandomScene()
+    {
+        string selectedScene = GetRandomSceneByWeight();
+        StartCoroutine(LoadSceneAsync(selectedScene));
+    }
+
     public void LoadNextScene(string _sceneName)
     {
         if(_sceneName == "05. MindPlace")
@@ -22,10 +49,61 @@ public class GameSceneManager : MonoBehaviour, IGameSceneManager
     {
         return SceneManager.GetActiveScene().name == _sceneName;
     }
+
+
+    private string GetRandomSceneByWeight()
+    {
+        float totalWeight = 0f;
+        foreach (var scene in weightedScenes)
+            totalWeight += scene.weight;
+
+        float randomValue = Random.value * totalWeight;
+        float current = 0f;
+
+        foreach (var scene in weightedScenes)
+        {
+            current += scene.weight;
+            if (randomValue <= current)
+                return scene.sceneName;
+        }
+
+        return weightedScenes[0].sceneName;
+    }
+
+    private IEnumerator LoadSceneAsync(string sceneName)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        while (!asyncLoad.isDone)
+            yield return null;
+
+        activeScenes.Add(sceneName);
+    }
+
+    public void UnloadLastScene()
+    {
+        if (activeScenes.Count == 0)
+        {           
+            return;
+        }
+
+        string lastScene = activeScenes[activeScenes.Count - 1];
+        StartCoroutine(UnloadSceneAsync(lastScene));
+    }
+
+    private IEnumerator UnloadSceneAsync(string sceneName)
+    {
+        AsyncOperation asyncUnload = SceneManager.UnloadSceneAsync(sceneName);
+        while (!asyncUnload.isDone)
+            yield return null;
+
+        activeScenes.Remove(sceneName);
+    }
 }
 
 public interface IGameSceneManager
 {
     bool IsCurrentScene(string _sceneName);
     void LoadNextScene(string _sceneName);
+    void LoadRandomScene();
+    void UnloadLastScene();
 }
---
+++ Assets/Scripts/WeightScene/WeightScene.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+[System.Serializable]
+public class WeightScene 
+{
+    public string sceneName; 
+    public float weight = 1f;
+}
