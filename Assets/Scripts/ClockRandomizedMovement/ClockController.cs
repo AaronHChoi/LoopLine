9a0cb71b07b140a532457ac0274487a11d6c80c5
julianKim11 <juliankim0911@gmail.com>
2025-10-22 16:02:02 -0300

    Remove stop-at-time feature from ClockController
    Eliminated the logic and serialized fields related to stopping the clock at a specific time in ClockController.cs. Updated the AS_Clocks.unity scene to retain stop parameters for clocks, but these are now unused in the script. Also made minor adjustments to a clock prefab's scale and name.

--- Assets/Scripts/ClockRandomizedMovement/ClockController.cs
+++ Assets/Scripts/ClockRandomizedMovement/ClockController.cs
@@ -14,15 +14,6 @@ namespace ClockRandomizedMovement
         [SerializeField] private float minSpeed = 5f;
         [SerializeField] private float maxSpeed = 100f;
 
-        [SerializeField] bool enableStop = false;
-        [SerializeField] int stopHour = 11;
-        [SerializeField] int stopMinute = 40;
-        [SerializeField] int stopSecond = 0;
-        [SerializeField] float stopDuration = 10f;
-
-        bool isStopped = false;
-        float stopTimer = 0f;
-
         private float _timeSpeed;
         private float _elapsed;
         private System.DateTime _startTime;
@@ -44,47 +35,16 @@ namespace ClockRandomizedMovement
 
         void Update()
         {
-            if (isStopped)
-            {
-                HandleStopDuration();
-                return;
-            }
-
             _elapsed += Time.deltaTime * _timeSpeed;
             DateTime t = _startTime.AddSeconds(_elapsed);
 
-            if(enableStop && ShouldStopAtTime(t))
-            {
-                isStopped = true;
-                stopTimer = 0f;
-                return;
-            }
-
-            UpdateClockHands(t);
-        }
-        private void UpdateClockHands(DateTime time)
-        {
-            float hourAngle = (time.Hour % 12 + time.Minute / 60f) * 30f;
-            float minuteAngle = (time.Minute + time.Second / 60f) * 6f;
-            float secondAngle = time.Second * 6f;
+            float hourAngle = (t.Hour % 12 + t.Minute / 60f) * 30f;
+            float minuteAngle = (t.Minute + t.Second / 60f) * 6f;
+            float secondAngle = t.Second * 6f;
 
             hourHand.localRotation = Quaternion.Euler(0, 0, -hourAngle);
             minuteHand.localRotation = Quaternion.Euler(0, 0, -minuteAngle);
             secondHand.localRotation = Quaternion.Euler(0, 0, -secondAngle);
-        }
-        private void HandleStopDuration()
-        {
-            stopTimer += Time.deltaTime;
-
-            if (stopTimer >= stopDuration)
-            {
-                isStopped = false;
-                _elapsed += 1;
-            }
-        }
-        private bool ShouldStopAtTime(DateTime time)
-        {
-            return time.Hour == stopHour && time.Minute == stopMinute && time.Second == stopSecond;
         }
     }
 }
