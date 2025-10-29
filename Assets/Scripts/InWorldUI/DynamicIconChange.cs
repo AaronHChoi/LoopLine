d980d7be286c0e5bf962e6649c76772fa99b12f7
Mateotorija <mateotorija@gmail.com>
2025-10-29 12:37:51 -0300

    remplace el item interact por base item interact

--- Assets/Scripts/InWorldUI/DynamicIconChange.cs
+++ Assets/Scripts/InWorldUI/DynamicIconChange.cs
@@ -11,13 +11,15 @@ namespace InWorldUI
         [SerializeField] private Sprite interactIcon;   // Icon for when canBePicked = false
 
         private ItemInteract _itemInteract;
+        private BaseItemInteract _baseInteract;
 
         private void Awake()
         {
             // Find the ItemInteract script in the root of the parent object
             _itemInteract = GetComponentInParent<ItemInteract>();
+            _baseInteract = GetComponent<BaseItemInteract>();
 
-            if (_itemInteract == null)
+            if (_itemInteract == null && _baseInteract == null)
             {
                 Debug.LogError($"[DynamicIconChange] No ItemInteract found in parent for {gameObject.name}");
             }
@@ -30,10 +32,13 @@ namespace InWorldUI
 
         private void LateUpdate()
         {
-            if (_itemInteract == null || iconImage == null) return;
+            if (_itemInteract == null || iconImage == null || _baseInteract) return;
 
             // Switch icon depending on canBePicked
-            iconImage.sprite = _itemInteract.canBePicked ? grabIcon : interactIcon;
+            if (_baseInteract != null ) 
+                iconImage.sprite = _baseInteract.canBePicked ? grabIcon : interactIcon;
+            if (_itemInteract != null )
+                iconImage.sprite = _itemInteract.canBePicked ? grabIcon : interactIcon;
         }
     }
 }
