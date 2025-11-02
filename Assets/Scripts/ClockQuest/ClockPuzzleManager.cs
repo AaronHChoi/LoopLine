6cb565ab8d4997265e52b18553a7be88ec2b76fe
julianKim11 <juliankim0911@gmail.com>
2025-11-02 17:55:35 -0300

    Refactor UIManager and improve ClockPuzzleManager logic
    Removed unused UI text functionality and related dependencies from UIManager, simplifying its interface and implementation. Updated ClockPuzzleManager to prevent multiple completion triggers and improved sound playback logic. Added BoxColliders to objects in AS_Clocks.unity for better interaction handling. Minor scene updates to Train and MindPlace for object management.

--- Assets/Scripts/ClockQuest/ClockPuzzleManager.cs
+++ Assets/Scripts/ClockQuest/ClockPuzzleManager.cs
@@ -9,6 +9,9 @@ public class ClockPuzzleManager : MonoBehaviour
     [SerializeField] int targetMinute;
     [SerializeField] GenericMove doorHandlerBase;
     [SerializeField] SoundData complete;
+
+    bool firstTime = false;
+
     private void Start()
     {
         if (GameManager.Instance.GetCondition(GameCondition.IsClockQuestComplete))
@@ -38,10 +41,16 @@ public class ClockPuzzleManager : MonoBehaviour
         int currenHour = int.Parse(timeParts[0]);
         int currentMinute = int.Parse(timeParts[1]);
 
-        if (currenHour == targetHour && currentMinute == targetMinute)
+        if (currenHour == targetHour && currentMinute == targetMinute && !firstTime)
         {
             RevealObject();
-            SoundManager.Instance.PlayQuickSound(complete);
+
+            SoundManager.Instance.CreateSound()
+                .WithSoundData(complete)
+                .WithSoundPosition(transform.position)
+                .Play();
+
+            firstTime = true;
         }
     }
     private void RevealObject()
--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -4,27 +4,18 @@ using UnityEngine;
 public class UIManager : Singleton<UIManager>, IUIManager
 {
     [SerializeField] private TextMeshProUGUI contador_provicional;
-    [SerializeField] private TextMeshProUGUI uiText;
     [SerializeField] GameObject pauseManager;
     bool isCursorVisible = false;
     [SerializeField] GameObject tutorialClockUI;
 
-    IGameSceneManager gameSceneManager;
-    ITimeProvider timeManager;
     ICrosshairFade crosshairFade;
     IGameStateController gameController;
     protected override void Awake()
     {
         base.Awake();
 
-        timeManager = InterfaceDependencyInjector.Instance.Resolve<ITimeProvider>();
         gameController = InterfaceDependencyInjector.Instance.Resolve<IGameStateController>();
         crosshairFade = InterfaceDependencyInjector.Instance.Resolve<ICrosshairFade>();
-        gameSceneManager = InterfaceDependencyInjector.Instance.Resolve<IGameSceneManager>();
-    }
-    private void Start()
-    {
-        uiText.gameObject.SetActive(false);
     }
     private void OnEnable()
     {
@@ -46,36 +37,10 @@ public class UIManager : Singleton<UIManager>, IUIManager
         pauseManager.SetActive(!pauseManager.activeSelf);
         UpdateCursorState();
     }
-    void Update()
-    {
-        if (gameSceneManager != null && gameSceneManager.IsCurrentScene("04. Train"))
-        {
-            ShowLoopTime();
-        }
-    }
-    private void ShowLoopTime()
-    {
-        if(timeManager != null)
-            contador_provicional.text = GetFormattedLoopTime(timeManager.LoopTime);
-    }
-    public string GetFormattedLoopTime(float loopTime)
-    {
-        return Mathf.FloorToInt(loopTime).ToString();
-    }
-    public void ShowUIText(string _message)
-    {
-        uiText.gameObject.SetActive(true);
-        uiText.text = _message;
-    }
-    public void HideUIText()
-    {
-        uiText.gameObject.SetActive(false);
-    }
     public void ShowCrossHairFade(bool show)
     {
         crosshairFade.ShowCrosshair(show);
     }
-
     void UpdateCursorState()
     {
         bool shouldShowCursor = pauseManager.activeInHierarchy;
@@ -90,8 +55,6 @@ public class UIManager : Singleton<UIManager>, IUIManager
 }
 public interface IUIManager
 {
-    void ShowUIText(string _message);
-    void HideUIText();
     void ShowCrossHairFade(bool show);
     void ShowClockTutorial(bool show);
 }
