dee862f0d9beff69b34c84c918787414550d7171
julianKim11 <juliankim0911@gmail.com>
2025-12-17 15:13:22 -0300

    Update FinalTrigger.cs

--- Assets/Scripts/FinalQuest/FinalTrigger.cs
+++ Assets/Scripts/FinalQuest/FinalTrigger.cs
@@ -9,16 +9,21 @@ public class FinalTrigger : MonoBehaviour
 
     ICinematicManager cinematicManager;
     IPlayerStateController playerStateController;
+    IGameSceneManager gameSceneManager;
 
     private void Awake()
     {
         cinematicManager = InterfaceDependencyInjector.Instance.Resolve<ICinematicManager>();
         playerStateController = InterfaceDependencyInjector.Instance.Resolve<IPlayerStateController>();
+        gameSceneManager = InterfaceDependencyInjector.Instance.Resolve<IGameSceneManager>();
     }
     private void OnTriggerEnter(Collider other)
     {
         playerStateController.StateMachine.TransitionTo(playerStateController.CinematicState);
-        cinematicManager.PlayCinematic(succesCinematic);
+        cinematicManager.PlayCinematic(succesCinematic, () =>
+        {
+
+        });
 
     }
 }
