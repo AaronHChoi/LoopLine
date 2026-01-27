ca828b2ba6f9847d2ba21cf4923b6483df9c751b
julianKim11 <juliankim0911@gmail.com>
2026-01-27 17:21:46 -0300

    visibilityController to Core

---
+++ Assets/Scripts/Core/Utility/VisibilityController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Core.Utilities
+{
+    public class VisibilityController : MonoBehaviour
+    {
+        private void Start()
+        {
+            SetChildrenActive(false);
+        }
+        public void SetChildrenActive(bool active)
+        {
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(active);
+            }
+        }
+        public void DeactivateSelfWithDelay(float delay)
+        {
+            DelayUtility.Instance.Delay(delay, () => gameObject.SetActive(false));
+        }
+    } 
+}
