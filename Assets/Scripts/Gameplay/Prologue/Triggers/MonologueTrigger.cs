a210e15048fdf8fdc7f89a859dd380b2247dd5b1
julianKim11 <juliankim0911@gmail.com>
2026-02-15 16:43:30 -0300

    Add namespace to MonologueTrigger and update scene refs
    Move MonologueTrigger into the Gameplay.Prologue namespace and adjust formatting. Update AS_NPC_PROLOGUE.unity to reference the updated script GUIDs / component fileIDs so the scene points to the renamed/moved MonoBehaviour. No gameplay logic changes intended.

--- Assets/Scripts/Gameplay/Prologue/Triggers/MonologueTrigger.cs
+++ Assets/Scripts/Gameplay/Prologue/Triggers/MonologueTrigger.cs
@@ -2,25 +2,28 @@ using UnityEngine;
 using Core.Utilities;
 using Core.DependencyInjection;
 
-public class MonologueTrigger : BaseTrigger
+namespace Gameplay.Prologue
 {
-    IMonologueSpeaker monologueSpeaker;
+    public class MonologueTrigger : BaseTrigger
+    {
+        IMonologueSpeaker monologueSpeaker;
 
-    private void Awake()
-    {
-        monologueSpeaker = InterfaceDependencyInjector.Instance.Resolve<IMonologueSpeaker>();
-    }
-    private void Start()
-    {
-        if (GameManager.Instance.TrainLoop != 1)
+        private void Awake()
         {
-            this.gameObject.SetActive(false);
+            monologueSpeaker = InterfaceDependencyInjector.Instance.Resolve<IMonologueSpeaker>();
         }
-    }
-    protected override void OnTriggerEnter(Collider other)
-    {
-        monologueSpeaker.StartMonologue(Events.LOOP2_Monologue);
+        private void Start()
+        {
+            if (GameManager.Instance.TrainLoop != 1)
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
+        protected override void OnTriggerEnter(Collider other)
+        {
+            monologueSpeaker.StartMonologue(Events.LOOP2_Monologue);
 
-        base.OnTriggerEnter(other);
-    }
+            base.OnTriggerEnter(other);
+        }
+    } 
 }
